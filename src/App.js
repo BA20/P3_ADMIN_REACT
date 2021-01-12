e2e0113201683fddd732b01e73a8f593554227ba
Bernardo Alves <bjca1998@gmail.com>
2021-01-12 13:52:16 +0000

    auth resolve

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ import Home from "./pages/Home";
 import {
   BrowserRouter as Router,
   Link,
-  Redirect,
   Route,
   Switch,
+  Redirect,
 } from "react-router-dom";
 import Atletas from "./pages/Atletas";
 import Treinadores from "./pages/Treinadores";
@@ -54,216 +54,303 @@ export default function App() {
   Axios.defaults.withCredentials = true;
 
   if (localStorage.getItem("token") !== null) {
-    console.log("Autenticado");
-    setisAuth(true);
-  }
-
-  const login = () => {
-    if (password.length <= 1) {
-      setLoginStatus("Campos Vazios!");
-      setAlert(true);
-    }
-    if (username.length <= 1) {
-      setLoginStatus("Campos Vazios!");
-      setAlert(true);
-    } else {
-      Axios.post(
-        `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/login`,
-        {
-          //Axios.post(`http://localhost:5050/login`, {
-          username: username,
-          password: password,
-        }
-      ).then((response) => {
-        if (!response.data.auth) {
-          setLoginStatus(response.data.message);
-          setAlert(true);
-        } else {
-          console.log(response.data.message);
-          localStorage.setItem("token", response.data.token);
-          setLoginStatus(true);
-          setisAuth(true);
-        }
-      });
-    }
-  };
-
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <div
-            className="App"
-            style={{
-              backgroundImage: `url(${background})`,
-              backgroundColor: "#007BFF",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              height: "100vh",
-              margin: "auto",
-              display: "flex",
-              direction: "column",
-            }}
-          >
-            {isAuth ? (
-              <Redirect path="/home" />
-            ) : (
-              <>
-                <div>
-                  <Alert
-                    variant="danger"
-                    onClose={() => setAlert(false)}
-                    dismissible
-                    show={alert}
-                  >
-                    <Alert.Heading>{loginStatus}</Alert.Heading>
-                  </Alert>
-                </div>
+    Axios.get(
+      `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/isUserAuth`,
+      {
+        headers: {
+          "x-access-token": localStorage.getItem("token"),
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+      if (res.data.auth === true) {
+        setisAuth(true);
+        return (
+          <Router>
+            <Switch>
+              <Route path="/home" exact>
                 <div
-                  className="login"
+                  className="App"
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    backgroundImage: `url(${background})`,
+                    backgroundColor: "#007BFF",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
                     height: "100vh",
-                    padding: "0px",
                     margin: "auto",
-                    width: "50%",
-                    backgroundColor: "rgba(255,255,255, 0.9)",
+                    display: "flex",
+                    direction: "column",
                   }}
                 >
-                  <Container
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Row>
-                      <Col>
-                        <Image
-                          src={logo}
-                          roundedCircle
-                          style={{
-                            padding: "20px",
-                          }}
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="login">
-                        <Form
-                          {...layout}
-                          name="basic"
-                          initialValues={{
-                            remember: true,
-                          }}
-                          className="login"
-                          style={{
-                            paddingTop: "30%",
-                          }}
-                        >
-                          <Form.Item
-                            prefix={<UserOutlined />}
-                            label="Username "
-                            name="username"
-                          >
-                            <Input
-                              onChange={(e) => {
-                                setUsername(e.target.value);
-                              }}
-                            />
-                          </Form.Item>
-
-                          <Form.Item label="Password" name="password">
-                            <Input
-                              type="password"
-                              onChange={(e) => {
-                                setPassword(e.target.value);
-                              }}
-                            />
-                          </Form.Item>
-
-                          <Form.Item {...tailLayout}>
-                            <Button
-                              type="primary"
-                              htmlType="submit"
-                              style={{
-                                backgroundColor: "#001145",
-                                borderColor: "#001145",
-                              }}
-                              onClick={login}
-                            >
-                              <Link to="/home" isAuth={isAuth}>
-                                Login
-                              </Link>
-                            </Button>
-                          </Form.Item>
-                        </Form>
-                      </Col>
-                    </Row>
-                  </Container>
+                  <Home></Home>
                 </div>
-              </>
-            )}
-          </div>
-        </Route>
-        <Route path="/atletas">
-          <Atletas />
-        </Route>
-        <Route path="/treinadores">
-          <Treinadores />
-        </Route>
-        <Route path="/eventos">
-          <Eventos />
-        </Route>
-        <Route path="/avaliacao">
-          <Avaliacao />
-        </Route>
-        <Route path="/exercicios">
-          <Exercicios />
-        </Route>
-        <Route path="/outros">
-          <Outros />
-        </Route>
-      </Switch>
+              </Route>
+              <Route path="/atletas">
+                <Atletas />
+              </Route>
+              <Route path="/treinadores">
+                <Treinadores />
+              </Route>
+              <Route path="/eventos">
+                <Eventos />
+              </Route>
+              <Route path="/avaliacao">
+                <Avaliacao />
+              </Route>
+              <Route path="/exercicios">
+                <Exercicios />
+              </Route>
+              <Route path="/outros">
+                <Outros />
+              </Route>
+            </Switch>
 
-      <ProtectedRoute
-        path="/home"
-        component={Home}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/atletas"
-        component={Atletas}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/treinadores"
-        component={Treinadores}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/eventos"
-        component={Eventos}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/avaliacao"
-        component={Avaliacao}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/exercicios"
-        component={Exercicios}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-      <ProtectedRoute
-        path="/outros"
-        component={Outros}
-        isAuth={isAuth}
-      ></ProtectedRoute>
-    </Router>
-  );
+            <ProtectedRoute
+              path="/home"
+              component={Home}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/atletas"
+              component={Atletas}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/treinadores"
+              component={Treinadores}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/eventos"
+              component={Eventos}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/avaliacao"
+              component={Avaliacao}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/exercicios"
+              component={Exercicios}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/outros"
+              component={Outros}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+          </Router>
+        );
+      } else {
+        setisAuth(false);
+
+        const login = () => {
+          if (password.length <= 1) {
+            setLoginStatus("Campos Vazios!");
+            setAlert(true);
+          }
+          if (username.length <= 1) {
+            setLoginStatus("Campos Vazios!");
+            setAlert(true);
+          } else {
+            Axios.post(
+              `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/login`,
+              {
+                //Axios.post(`http://localhost:5050/login`, {
+                username: username,
+                password: password,
+              }
+            ).then((response) => {
+              if (!response.data.auth) {
+                setLoginStatus(response.data.message);
+                setAlert(true);
+              } else {
+                console.log(response.data.message);
+                localStorage.setItem("token", response.data.token);
+                setLoginStatus(true);
+                setisAuth(true);
+              }
+            });
+          }
+        };
+
+        return (
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                <div
+                  className="App"
+                  style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundColor: "#007BFF",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    height: "100vh",
+                    margin: "auto",
+                    display: "flex",
+                    direction: "column",
+                  }}
+                >
+                  <>
+                    <div>
+                      <Alert
+                        variant="danger"
+                        onClose={() => setAlert(false)}
+                        dismissible
+                        show={alert}
+                      >
+                        <Alert.Heading>{loginStatus}</Alert.Heading>
+                      </Alert>
+                    </div>
+                    <div
+                      className="login"
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100vh",
+                        padding: "0px",
+                        margin: "auto",
+                        width: "50%",
+                        backgroundColor: "rgba(255,255,255, 0.9)",
+                      }}
+                    >
+                      <Container
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Row>
+                          <Col>
+                            <Image
+                              src={logo}
+                              roundedCircle
+                              style={{
+                                padding: "20px",
+                              }}
+                            />
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col className="login">
+                            <Form
+                              {...layout}
+                              name="basic"
+                              initialValues={{
+                                remember: true,
+                              }}
+                              className="login"
+                              style={{
+                                paddingTop: "30%",
+                              }}
+                            >
+                              <Form.Item
+                                prefix={<UserOutlined />}
+                                label="Username "
+                                name="username"
+                              >
+                                <Input
+                                  onChange={(e) => {
+                                    setUsername(e.target.value);
+                                  }}
+                                />
+                              </Form.Item>
+
+                              <Form.Item label="Password" name="password">
+                                <Input
+                                  type="password"
+                                  onChange={(e) => {
+                                    setPassword(e.target.value);
+                                  }}
+                                />
+                              </Form.Item>
+
+                              <Form.Item {...tailLayout}>
+                                <Button
+                                  type="primary"
+                                  htmlType="submit"
+                                  style={{
+                                    backgroundColor: "#001145",
+                                    borderColor: "#001145",
+                                  }}
+                                  onClick={login}
+                                >
+                                  <Link to="/home" isAuth={isAuth}>
+                                    Login
+                                  </Link>
+                                </Button>
+                              </Form.Item>
+                            </Form>
+                          </Col>
+                        </Row>
+                      </Container>
+                    </div>
+                  </>
+                </div>
+              </Route>
+              <Route path="/atletas">
+                <Atletas />
+              </Route>
+              <Route path="/treinadores">
+                <Treinadores />
+              </Route>
+              <Route path="/eventos">
+                <Eventos />
+              </Route>
+              <Route path="/avaliacao">
+                <Avaliacao />
+              </Route>
+              <Route path="/exercicios">
+                <Exercicios />
+              </Route>
+              <Route path="/outros">
+                <Outros />
+              </Route>
+            </Switch>
+
+            <ProtectedRoute
+              path="/home"
+              component={Home}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/atletas"
+              component={Atletas}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/treinadores"
+              component={Treinadores}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/eventos"
+              component={Eventos}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/avaliacao"
+              component={Avaliacao}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/exercicios"
+              component={Exercicios}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/outros"
+              component={Outros}
+              isAuth={isAuth}
+            ></ProtectedRoute>
+          </Router>
+        );
+      }
+    });
+  }
 }
