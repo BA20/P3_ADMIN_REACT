643b9c45bbfd63f6ae19967bd6c21b02cc582057
Bernardo Alves <bjca1998@gmail.com>
2021-01-18 22:05:46 +0000

    table get users

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,50 +1,35 @@
 import * as React from "react";
+import useState from "react";
 import { withRouter } from "react-router-dom";
 import { Layout } from "antd";
 import TopBar from "./components/TopBar";
 import NavBar from "./components/NavBar";
 import background from "./components/images/background-login.jpg";
-//import Axios from "axios";
+import Axios from "axios";
 
 import { DataGrid } from "@material-ui/data-grid";
 //  import jwt from "jwt-decode";
 const { Header, Sider, Content } = Layout;
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue("firstName") || ""} ${
-        params.getValue("lastName") || ""
-      }`,
-  },
-];
+const data = () => {
+  Axios.get(
+    `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/users`
+  ).then((response) => {
+    console.log(response.data);
+  });
+};
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 function Users() {
+  // const [UsersList, setUsersList] = useState([]);
+
+  const columns = [
+    { field: "idUser", headerName: "ID", width: 70 },
+    { field: "Name", headerName: "Nome", width: 130 },
+    { field: "Email", headerName: "Email", width: 130 },
+    { field: "PhoneNumber", headerName: "Nº Tlm", width: 130 },
+    { field: "Tipo", headerName: "Tipo", width: 130 },
+  ];
+
   return (
     <div
       className="App"
@@ -95,7 +80,7 @@ function Users() {
                 width: "100%",
               }}
             >
-              <DataGrid rows={rows} columns={columns} pageSize={10} />
+              <DataGrid rows={data} columns={columns} pageSize={10} />
             </div>
           </Content>
         </Layout>
